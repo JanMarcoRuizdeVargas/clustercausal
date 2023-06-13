28b3125b566492ad45418144ca9c428b6ebf5529
JanMarcoRuizdeVargas <68998652+JanMarcoRuizdeVargas@users.noreply.github.com>
2023-06-13 19:38:01 +0200

    Adding basic code structure for clustcausal package

--- clustcausal/algs/alg_clust_pc.py
+++
@@ -1,5 +0,0 @@
-import pcalg
-
-class ClustPC():
-    def __init__():
-        pass
---
+++ clustcausal/utils/utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+def is_valid_clustering(cdag, causal_graph):
+    """
+    Checks if a CDAG is a valid clustering of a causal graph
+
+    Parameters
+    ----------
+    cdag : CDAG
+        instance of CDAG class
+    causal_graph : CausalGraph
+        instance of CausalGraph class
+    """
+    pass
