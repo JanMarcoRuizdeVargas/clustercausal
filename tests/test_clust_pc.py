26dc4039747e7d67fc77e1785abe423e55412272
JanMarcoRuizdeVargas <68998652+JanMarcoRuizdeVargas@users.noreply.github.com>
2023-06-29 23:51:40 +0200

    adding test templates

---
+++ tests/test_clust_pc.py
@@ -0,0 +1,31 @@
+import numpy as np
+import causallearn
+from causallearn.search.ConstraintBased.PC import pc
+from clustcausal.clusterdag.cluster_dag import CDAG
+from clustcausal.algs.clust_pc import ClustPC
+
+def test_clust_pc_correct_output():
+    """
+    TODO based on noteboks, write a testcase generating and checking a couple cpc runs
+    !!! make a test function for each method of CPC
+    """
+    pass
+
+def test_clust_pc_to_base_pc():
+    """
+    TODO based on noteboks, write a testcase generating and checking a couple cpc runs
+    against the baseline pc algorithm
+    """
+    pass
+
+def test_clust_pc_initialization():
+    pass
+
+def test_clust_pc_intra_phase():
+    pass
+
+def test_clust_pc_inter_phase():
+    pass
+
+def test_clust_pc_edge_orientation():
+    pass
