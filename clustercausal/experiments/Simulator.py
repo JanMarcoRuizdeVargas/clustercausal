4b39a30d2e5a40c79078375429dfeb8be70bd59e
JanMarcoRuizdeVargas <jan.marco@ruizdevargas.de>
2023-07-20 12:38:37 +0200

    finished simulator, going into debugging

--- clustercausal/experiments/Simulator.py
+++ clustercausal/experiments/Simulator.py
@@ -1,10 +1,13 @@
 import causallearn
 import castle
 import numpy as np
+import itertools
 
 from causallearn.graph.GraphClass import CausalGraph
 
 from castle.datasets.simulator import IIDSimulation, DAG
+
+from clustercausal.clusterdag.ClusterDAG import ClusterDAG
 
 
 class Simulator:
@@ -41,11 +44,34 @@ class Simulator:
 
     def run(self):
         """
-        Run a simulation
+        Run the simulator and get cluster_graph, true_dag,
+        cluster_mapping and data
+        """
+        self.true_dag, self.data = self.create_dag_and_data()
+        cluster_graph, self.true_dag, cluster_mapping = self.generate_clustering()
+        self.cluster_graph = cluster_graph
+        self.cluster_mapping = cluster_mapping
+
+        cluster_edges = self.cluster_graph.G.get_graph_edges()
+        cluster_edges_names = []
+        for edge in cluster_edges:
+            node1_name = edge.get_node1().get_name()
+            node2_name = edge.get_node2().get_name()
+            cluster_edges_names.append((node1_name, node2_name))
+        # Create a ClusterDAG object
+        simulated_cluster_dag = ClusterDAG(self.cluster_mapping, cluster_edges_names)
+        simulated_cluster_dag.true_dag = self.true_dag
+        simulated_cluster_dag.data = self.data
+        return simulated_cluster_dag
+
+    def create_dag_and_data(self):
+        """
+        Create dag and data
         """
         if self.true_dag is None:
             self.true_dag = self.generate_dag()
         self.data = self.generate_data()
+        # TODO put cluster_graph in here too
         return self.true_dag, self.data
 
     def generate_dag(self):
@@ -139,13 +165,43 @@ class Simulator:
             n_c_edges = np.random.randint(
                 low=n_clusters - 1, high=n_clusters * (n_clusters - 1) / 2
             )
+
         cluster_simulator = Simulator(
             n_nodes=n_clusters, n_edges=n_c_edges, dag_method=self.dag_method
         )
-        cluster_graph, _ = cluster_simulator.run()
-
+        cluster_graph, _ = cluster_simulator.create_dag_and_data()
         cluster_names = [f"C{i+1}" for i in range(n_clusters)]
+        cluster_graph.nodes_names = cluster_names
+
+        # Give each cluster a probability of being chosen
+        cluster_probabilities = np.random.dirichlet(np.ones(n_clusters), size=1)[0]
+        cluster_mapping = {}
+        # Make cluster mapping
+        for cluster_name in cluster_names:
+            cluster_mapping[cluster_name] = []
         for node_name in self.true_dag.node_names:
-            pass
-        # TODO finish this
-        return cluster_graph
+            chosen_cluster = np.random.choice(cluster_names, p=cluster_probabilities)
+            cluster_mapping[chosen_cluster] += [node_name]
+        # Adjust true_dag such that cluster_graph is admissible
+        for cluster1, cluster2 in itertools.combinations(cluster_names, 2):
+            for node1 in cluster_mapping[cluster1]:
+                for node2 in cluster_mapping[cluster2]:
+                    c1 = ClusterDAG.get_node_by_name(cluster1, cluster_graph)
+                    c2 = ClusterDAG.get_node_by_name(cluster2, cluster_graph)
+                    n1 = ClusterDAG.get_node_by_name(node1, self.true_dag)
+                    n2 = ClusterDAG.get_node_by_name(node2, self.true_dag)
+                    c1_indice = cluster_graph.G.node_map[c1]
+                    c2_indice = cluster_graph.G.node_map[c2]
+                    n1_indice = self.true_dag.G.node_map[n1]
+                    n2_indice = self.true_dag.G.node_map[n2]
+                    # Reorient edge n1 -> n2 to n1 <- n2 if c1 <- c2
+                    if (
+                        cluster_graph.G.graph[c1_indice, c2_indice] == 1
+                        and cluster_graph.G.graph[c2_indice, c1_indice] == -1
+                        and self.true_dag.G.graph[n1_indice, n2_indice] == -1
+                        and self.true_dag.G.graph[n2_indice, n1_indice] == 1
+                    ):
+                        self.true_dag.G.graph[n1_indice, n2_indice] = 1
+                        self.true_dag.G.graph[n2_indice, n1_indice] = -1
+
+        return cluster_graph, self.true_dag, cluster_mapping
